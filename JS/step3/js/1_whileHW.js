ff9b6793bac0ad1a7dab9a62c4d842bb2ccedaa0
LYH <yh6194@naver.com>
2022-11-21 13:02:42 +0900

    for 종료

--- JS/step3/js/1_whileHW.js
+++ JS/step3/js/1_whileHW.js
@@ -1,16 +1,30 @@
 //todo. end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하고, prompt에는 몇번 수행 했는가에 대한 "x 번째 입력" 이 표시되야함. 
 
-function whileSum(){
-    var count = 1;
-    var sum = 0;
+// function whileSum(){//내 풀이
+//     var count = 1;
+//     var sum = 0;
+//     while(true){
+//         var num = prompt(count + '번째 입력');
+//         if(num=='end'){
+//             break;
+//         }
+//         count++;
+//         sum += Number(num);
+//         document.write('현재까지의 총합 : ' + sum + '<br>');
+//     }
+//     alert('입력이 종료되었습니다.');
+// }
+function sumNumb(){//강사님 풀이
+    var i = 1;
+    var result = 0;
     while(true){
-        var num = prompt(count + '번째 입력');
-        if(num=='end'){
+        var numb = window.prompt(i + '번째 입력');
+        if(numb == 'end'){
+            alert('입력이 종료되었습니다.');
             break;
         }
-        count++;
-        sum += Number(num);
-        document.write('현재까지의 총합 : ' + sum + '<br>');
+        result += parseInt(numb);
+        document.write('현재까지의 총합 : ' + result + "<br>");
+        i++;
     }
-    alert('입력이 종료되었습니다.');
 }
--- JS/step3/js/3_for.js
+++ JS/step3/js/3_for.js
@@ -13,3 +13,65 @@
     3. 참일경우 문장 실행 > 4. 종결식 실행 > 5. 2단계
 
 */
+//! 단일 for 문
+
+// *case.1 이름을 10번 출력해보자.
+function namePrint10(){
+    var userName = "Joy";
+    for(var i = 1; i <= 10; i++){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+// *case.2 이름을 1000번 출력해보자.
+function namePrint1000(){
+    var userName = "Joy";
+    for(var i = 1; i <= 1000; i++){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+// *case.3 1000번중에 홀수번째만 출력
+function namePrint500(){
+    var userName = "Joy";
+    for(var i = 1; i <= 1000; i+=2){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+//todo. 머리속에 생각나는 1 ~ 10까지 출력되는 반복문
+function answer1(){
+    for(var i = 1; i <= 10; i++){
+        document.write(i + '<br>');
+    }
+}
+function answer2(){
+    for(var i = 10; i >= 1; i--){
+        document.write(i + '<br>');
+    }
+}
+function answer3(){
+    for(var i = 2, j = 3; i <= 20; i += 2, j += 3){
+        document.write(i + ',' + j + '<br>');
+    }
+}
+function answer4(){
+
+}
+function answer5(){
+
+}
+// *case.4 자신이 좋아하는 과일 4개를 배열로 배치하고, alert 출력
+function favorFruit(){
+    var array = ['딸기', '자두', '복숭아', '살구', '한라봉', '귤'];
+    for(var i = 0; i < array.length; i++){
+        document.write(array[i] + '<br>');
+    }
+}
+//todo. 지금까지 배웠던 JS단원명을 배열을 활용해 출력
+//? 변수, 연산자, 자료형, 형변환, 조건문if, 조건문switch, 반복문 while, 반복문 for
+function arrayFor(){
+    var data = ['변수', '연산자', '형변환', '조건문if', '조건문switch', '반복문while', '반복문for'];
+    // ? ECMA Script.
+    for (let i = 0; i < data.length; i++){ //? 상시변수 - 늘 변할 수 있는 변수
+        const element = data[i]; //? 비상시변수 - 개발자가 원할 시에만 변경해야 하는 변수(잠금?)
+        document.write((i+1) + '번째 내용 = ' + element + '<br>');
+    }
+}
