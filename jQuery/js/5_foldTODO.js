88c2426fbb9ffe3fced63e912620a167b83fe7ff
LYH <yh6194@naver.com>
2022-11-30 12:49:20 +0900

    Interval

---
+++ jQuery/js/5_foldTODO.js
@@ -0,0 +1,11 @@
+$(document).reday(function(){
+    gallery();//? 초기배치
+    //? n열 버튼 눌렀을때(열 매개변수, 너비, 높이)
+    //! 너비 높이는 1열에 들어가는 수치가 기준
+});
+function gallery(){
+    //? 기존에 풀었던 문제들처럼
+    //? image's 선언, length(forfor),xpos,xpos
+    //? for loop 잡아서 알아서 작동될 수 있는 logic
+    //? css 적용(기존에 했던것 left, top 말고도 width, height)
+}
---
+++ jQuery/js/6_appPrePend.js
@@ -0,0 +1,20 @@
+var $ex2 = null;
+var count = 0;
+
+$(document).ready(function(){
+    $ex2 = $("#ex2");
+    $ex2.html("<p>" + count + " - hi, hello </p>");//? html() <-> write() 함수와 유사
+    $(window).scroll(appendCount);
+    //! position - left, top, offset - x, y, if, target.height(); target.css("padding-top");
+    // $("#ex2append").click(appendCount);
+    $("#ex2prepend").click(prependCount);
+});
+
+function appendCount(){
+    count++;
+    $ex2.append("<p>" + count + " - hi, hello </p>");
+}
+function prependCount(){
+    count--;
+    $ex2.append("<p>" + count + " - hi, hello </p>");
+}
